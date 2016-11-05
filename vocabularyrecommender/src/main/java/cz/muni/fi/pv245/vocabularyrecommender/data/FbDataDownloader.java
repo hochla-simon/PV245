594f0fa14dd1a34e3734005deae27de77125251d
danielminarik <daniel.minarik@gmail.com>
2016-11-05 17:12:11 +0100

    updated FbDataDownloader

--- vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/data/FbDataDownloader.java
+++ vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/data/FbDataDownloader.java
@@ -8,46 +8,68 @@ package cz.muni.fi.pv245.vocabularyrecommender.data;
 import java.io.IOException;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
+import com.restfb.Parameter;
 import com.restfb.types.User;
 import com.restfb.Version;
 import com.restfb.exception.FacebookOAuthException;
 import com.restfb.json.JsonObject;
-import com.restfb.types.Page;
+import com.restfb.json.JsonArray;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+import org.json.simple.JSONArray;
+ 
 /**
  *
  * @author Daniel
  */
 public class FbDataDownloader {
     
-    public static void main(String[] args) throws IOException {
-        String MY_ACCESS_TOKEN = "EAACEdEose0cBAO5urxPG9hrAbegvzhvc3HSzTM4PZAa6dXqL17B0AL2XFUs0ZBqiAqeFl5WexyKoCZBAWY3IkYqwHVZCZAo6cAQZAVwc6vK9TWO8OM9FhLiNLuhZCtZB2ZCcmkyvjOiA7oDiRp0691QY7Hr484MZANY9bKIGesPfVqEwZDZD";
+    public static void main(String[] args) throws IOException, MalformedURLException {
+        String MY_ACCESS_TOKEN = "EAACEdEose0cBAE1oggAeA0olpuXyznsAV3rYciNIGzrCUov4ok7ZA11veeTOWAQZBZBsjoZAXSdNinFsZCwPyc6SCYGkDc119YZCCA3yayUplwvCrxzPdJIa7kzU6k8khhKbWW8rj8UL0F8Yo4lzkZBmymfmgiFlVZBcEFsrAnQd1AZDZD";
         downloadFbData(MY_ACCESS_TOKEN);
     }  
     
-    public static void downloadFbData(String token) {
+    public static void downloadFbData(String token) throws MalformedURLException, IOException {
         FacebookClient facebookClient;
         try {
             facebookClient = new DefaultFacebookClient(token, Version.VERSION_2_8);
             User user = facebookClient.fetchObject("me", User.class);
             System.out.println("User name: " + user.getName());
         } catch (FacebookOAuthException e) {
-            System.out.println(e.toString());
+            System.out.println("You have probably wrong token!\n" + e.toString());
             return;
         }
-        Collection<String> events = getEvents(facebookClient);
-//        for (String event : events) {
-//            System.out.println("EVENT:\n" + event);
-//        }
-        getLikedPagesFeed(facebookClient);
-        getMyFeed(facebookClient);
-        
+        saveToJson(getEvents(facebookClient), "events.json");
+        System.out.println("File events.json created");
+        saveToJson(getLikedPagesFeed(facebookClient), "pagesfeed.json");
+        System.out.println("File pagesfeed.json created");
+        saveToJson(getMyFeed(facebookClient), "feed.json");
+        System.out.println("File feed.json created");
     }
      
-    public static Collection<String> getEvents(FacebookClient facebookClient) {
+    private static void saveToJson(Collection<String> col, String fileName) throws UnsupportedEncodingException, FileNotFoundException, IOException {
+        JSONArray obj = new JSONArray();
+        for (String d : col) {
+            obj.add(d);
+        }
+        Writer out = new BufferedWriter(new OutputStreamWriter(
+            new FileOutputStream(fileName), "UTF-8"));
+        try {
+            out.write(obj.toJSONString());
+        } finally {
+            out.close();
+        }        
+    }
+
+    private static Collection<String> getEvents(FacebookClient facebookClient) {
         JsonObject eventsConnection = facebookClient.fetchObject("me/events", JsonObject.class);
         List<String> col = new ArrayList<>();
         for (int i=0; i<eventsConnection.getJsonArray("data").length(); i++) {
@@ -57,7 +79,7 @@ public class FbDataDownloader {
         return col;
     }
 
-    public static Collection<String> getLikedPagesFeed(FacebookClient facebookClient) {
+    private static Collection<String> getLikedPagesFeed(FacebookClient facebookClient) {
         JsonObject likesConnection = facebookClient.fetchObject("me/likes", JsonObject.class);
         List<String> col = new ArrayList<>();
         for (int i=0; i<likesConnection.getJsonArray("data").length(); i++) {
@@ -76,14 +98,16 @@ public class FbDataDownloader {
         return col;
     }
 
-    public static String getMyFeed(FacebookClient facebookClient) {
+    private static Collection<String> getMyFeed(FacebookClient facebookClient) {
+        List<String> col = new ArrayList<>();
         String feed = new String();
-        JsonObject pageConnection = facebookClient.fetchObject("me/feed", JsonObject.class);
+        JsonObject pageConnection = facebookClient.fetchObject("me/feed", JsonObject.class, Parameter.with("message", "utf8"));
         for (int j=0; j<pageConnection.getJsonArray("data").length(); j++) {
             if (pageConnection.getJsonArray("data").getJsonObject(j).has("message")) {
                 feed += pageConnection.getJsonArray("data").getJsonObject(j).getString("message") + "\n";
             }
         }
-        return feed;
+        col.add(feed);
+        return col;
     }
 }
--- vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/web/StartListener.java
+++ vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/web/StartListener.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import cz.muni.fi.pv245.vocabularyrecommender.data.FbDataDownloader;
 
 /**
  * Created by simon on 28. 10. 2016.
@@ -13,12 +14,12 @@ public class StartListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent ev) {
-        System.out.println("aplikace inicializována");
+        System.out.println("aplikace inicializovana");
         ServletContext servletContext = ev.getServletContext();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent ev) {
-        System.out.println("aplikace končí");
+        System.out.println("aplikace konci");
     }
 }
