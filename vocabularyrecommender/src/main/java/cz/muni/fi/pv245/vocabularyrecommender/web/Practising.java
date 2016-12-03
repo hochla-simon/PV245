ff5a272d46d298e25da073c9d0d8a400226a4805
Simon Hochla <simon.hochla@inqool.cz>
2016-12-03 18:50:45 +0100

    Practising page created

--- vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/web/Practising.java
+++ vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/web/Practising.java
@@ -9,8 +9,27 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/practising"})
 public class Practising extends HttpServlet {
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
+        req.setAttribute("word", "dog");
+        req.setAttribute("meaning", "a common animal with four legs, fur, and a tail. Dogs are kept as pets or trained to guard places");
+
+        req.getRequestDispatcher("/Practising.jsp").forward(req, res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        //aby fungovala čestina z formuláře
+        req.setCharacterEncoding("utf-8");
+
+        if (req.getParameter("button1") != null) {
+            System.out.println("Button 1 pressed.");
+        } else if (req.getParameter("button2") != null) {
+            System.out.println("Button 2 pressed.");
+        } else {
+            // ???
+        }
     }
 }
--- vocabularyrecommender/src/main/webapp/Practising.jsp
+++ vocabularyrecommender/src/main/webapp/Practising.jsp
@@ -4,6 +4,21 @@
     <title>Title</title>
 </head>
 <body>
+<script language="javascript">
+    function showDiv() {
+        document.getElementById('welcomeDiv').style.display = "block";
+    }
+</script>
+
+<div>What is the meaning of word <b>${word}</b>?</div>
+
+<div id="welcomeDiv" style="display:none;" class="answer_list">${meaning}</div>
+<input type="button" name="answer" value="Show meaning" onclick="javascript:showDiv();"/>
+
+<form action="${pageContext.request.contextPath}/practising" method="post">
+    <input type="submit" name="button1" value="Yes, I knew"/>
+    <input type="submit" name="button2" value="Oops, I was wrong"/>
+</form>
 
 </body>
 </html>
