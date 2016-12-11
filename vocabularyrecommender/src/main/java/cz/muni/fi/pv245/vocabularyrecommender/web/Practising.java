fa84c13a40bf83c597db4f5ddd261e7e834e2faf
Simon Hochla <simon.hochla@inqool.cz>
2016-12-11 22:35:25 +0100

    Practising page prepared to work with the words difficulty

--- vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/web/Practising.java
+++ vocabularyrecommender/src/main/java/cz/muni/fi/pv245/vocabularyrecommender/web/Practising.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -22,51 +23,102 @@ public class Practising extends HttpServlet {
         req.setCharacterEncoding("utf-8");
 
         HttpSession session = req.getSession(true);
-        Pair<String, String> word;
+
+        /**
+         * Pair of 'word' and its 'translation' via exaplanatory dictionary that is sent to the user
+         */
+        Pair<String, String> word = null;
+
+        /**
+         * Map of words (keys) and their corresponding translations (values) via explanatory dictionary.
+         * The map is sorted in the order respective to the words difficulty (see the Comparator).
+         */
         TreeMap<String, String> words = null;
 
-        if (req.getParameter("Yes, I knew") != null) {
-            words = (TreeMap<String, String>) session.getAttribute("words");
-            words.remove(words.firstKey());
+        /**
+         * Index to the words map pointing to the word that is sent to the user.
+         */
+        int index = 0;
 
-            if (words.isEmpty()) {
-                req.getRequestDispatcher("/RecommendEvents.jsp").forward(req, res);
-            }
-           
-            String firstKey = words.firstKey();
-            word = new Pair(firstKey, words.get(firstKey));
-        } else if (req.getParameter("Oops, I was wrong") != null) {
+        //The user pressed 'Yes, I knew' for the previous word
+        if (req.getParameter("Yes, I knew") != null) {
+            index = (int) session.getAttribute("index");
             words = (TreeMap<String, String>) session.getAttribute("words");
-            
-            if (words.size() > 1) {
-                words.remove(words.firstKey());
-            }
-            String firstKey = words.firstKey();
-            word = new Pair(firstKey, words.get(firstKey));
-            words.put(word.getKey(), word.getValue());
+
+            //redirection the user back to the recommender page after practising is finished
+            if (words.size() <= 1) {
+                req.getRequestDispatcher("/RecommendEvents.jsp").forward(req, res);
+                return;
+            }  
+
+            //removing the previous word (that the user has already mastered)
+            String key = (String) words.keySet().toArray()[index];
+            words.remove(key);
+
+            //determining the new index
+            index = ((words.keySet().size() - 1) + index) / 2;
+
+            //...and the new word
+            String newKey = (String)words.keySet().toArray()[index];
+            word = new Pair(newKey, words.get(newKey));
+
+        //The user pressed 'Oops, I was wrong' for the previous word
+        } else if (req.getParameter("Oops, I was wrong") != null) {
+            index = (int) session.getAttribute("index");
+            words = (TreeMap<String, String>) session.getAttribute("words");
+
+            //determining the new index
+            index = index / 2;
+
+            //...and the new word
+            String key = (String)words.keySet().toArray()[index];
+            word = new Pair(key, words.get(key));
+
+        //request came from the recommender page
         } else {
-            //request came from the recommender page
+            //datastructure obtained from the recommender page
             Map<Integer, HashMap<String, HashMap<String, String>>>  wordsMapOfMapsOfMaps =
                     (Map<Integer, HashMap<String, HashMap<String, String>>>) session.getAttribute("wordsMap");
+            //index pointing to the item selected by the user
             String item = req.getParameter("item");
 
+            //group of words user selected on the recommender page to learn
             HashMap<String, HashMap<String, String>> wordsMapOfMaps =
                     wordsMapOfMapsOfMaps.get(Integer.parseInt(item));
-            
-            words = new TreeMap<String, String>();
+
+            //comparator ordering the values of map using the words difficulty
+            Comparator<String> wordsComparator = new Comparator<String>() {
+                @Override public int compare(String o1, String o2) {
+                    if (o1.length() > o2.length()) {
+                        return 1;
+                    } else if (o1.length() < o2.length()) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            };
+            words = new TreeMap<>(wordsComparator);
+
+            //transfering words and their translations to the new datastructre
             for (Map.Entry pair : wordsMapOfMaps.entrySet()) {
                 for (Map.Entry pair2 : ((HashMap<String, String>)pair.getValue()).entrySet()) {
                     words.put((String) pair2.getKey(), (String) pair2.getValue());
                 }
             }
-             
-            String firstKey = words.firstKey();
-            word = new Pair(firstKey, words.get(firstKey));
+
+            //selection of the first word user is going to learn
+            //let's start with the moderate difficulty
+            int wordsCount = words.keySet().size();
+            index = wordsCount / 2;
+            String key = (String) words.keySet().toArray()[index];
+            word = new Pair(key, words.get(key));
         }
            
         session.setAttribute("word", word.getKey());
         session.setAttribute("words", words);
-
+        session.setAttribute("index", index);
+        
         req.setAttribute("meaning", word.getValue());
 
         req.getRequestDispatcher("/Practising.jsp").forward(req, res);
